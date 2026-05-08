59425ebba4c9e5d154bf5643a61d0b8e419d7da0
Saad Raza <sfurqanarif7528@gmail.com>
2026-05-08 16:29:50 +0500

    SFA Institute Registration Form

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import dotenv from "dotenv";
 dotenv.config();
+
 import express from "express";
 import cors from "cors";
 import nodemailer from "nodemailer";
@@ -53,7 +54,6 @@ Demo Class: ${DemoClass}
 });
 
 const PORT = process.env.PORT || 3000;
-
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
